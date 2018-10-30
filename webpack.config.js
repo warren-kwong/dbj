d46570421dc44801d92235cc67ecc82ed889b8aa
Warren <warren.p.kwong@gmail.com>
2018-10-30 14:47:13 -0700

    fix file structure / more setup

--- webpack.config.js
+++ webpack.config.js
@@ -1,13 +1,33 @@
+const path = require('path');
+
+const outputDirectory = '/client/dist';
+
 module.exports = {
+  entry: './client/src/index.js',
+  output: {
+    path: path.join(__dirname, outputDirectory),
+    filename: 'bundle.js'
+  },
   module: {
     rules: [
       {
-        test: /\.js$/,
+        test: /\.js[x]?$/,
         exclude: /node_modules/,
         use: {
           loader: 'babel-loader'
         }
+      },
+      {
+        test: /\.(css)$/,
+        use: ['style-loader', 'css-loader']
       }
     ]
+  },
+  devServer: {
+    port: 3000,
+    open: true,
+    proxy: {
+      '/api': 'http://localhost:8080'
+    }
   }
 };
